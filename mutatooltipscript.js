51fa73afbd7c1b3280e38e8b951f6e947e51a7c8
ondrejhlavac <ondrej.hlavac@outlook.com>
2015-05-06 05:47:51 +0200

    init-comit

---
+++ mutatooltipscript.js
@@ -0,0 +1,48 @@
+/**
+ * Created by Ondrej on 06/05/2015.
+ */
+
+onload = function (e) {
+
+    var tooltipWindow = document.getElementsByClassName('mt-tooltip');
+
+    for( i=0; i<tooltipWindow.length; i++ ) {
+        document.getElementsByClassName('mt-tooltip')[i].addEventListener('mouseover', tooltipHover, false);
+    }
+
+    for( p=0; p<tooltipWindow.length; p++ ) {
+        document.getElementsByClassName('mt-tooltip')[p].addEventListener('mouseout', tooltipOut);
+    }
+
+
+    function tooltipHover(){
+        var tooltipInfo = this.title;
+        var tooltipBox = document.createElement("SPAN");
+        tooltipBox.innerHTML = tooltipInfo;
+        tooltipBox.setAttribute("id", "mt-active")
+        this.appendChild(tooltipBox);
+    }
+
+    function tooltipOut(){
+        var tooltipBox = document.getElementById('mt-active');
+        this.removeChild(tooltipBox);
+    }
+
+    //document.getElementById("mt-tooltip").addEventListener("tooltipOver", tooltipOver);
+    //document.getElementById("mt-tooltip").addEventListener("tooltipOut", tooltipOut);
+
+    //function tooltipOver() {
+    //    document.getElementById("mt-tooltip").style.color = "red";
+    //}
+    //
+    //function tooltipOut() {
+    //    document.getElementById("mt-tooltip").style.color = "black";
+    //}
+}
+
+
+//var myTooltip = new Window();
+//
+//myTooltip.hover=function(){
+//    var tooltipInfo = e.title;
+//}
